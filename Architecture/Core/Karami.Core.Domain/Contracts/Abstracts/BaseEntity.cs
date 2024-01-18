0d659bc7ac714fa2f3ec013724694db006d5db90
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-01-18 20:04:59 +0330

    changed basic domain

--- Architecture/Core/Karami.Core.Domain/Contracts/Abstracts/BaseEntity.cs
+++ Architecture/Core/Karami.Core.Domain/Contracts/Abstracts/BaseEntity.cs
@@ -6,11 +6,11 @@ namespace Karami.Core.Domain.Contracts.Abstracts;
 
 public abstract class BaseEntity<TIdentity>
 {
-    public TIdentity Id { get; protected set; }
-    public TIdentity CreatedBy { get; protected set; }
+    public TIdentity Id         { get; protected set; }
+    public TIdentity CreatedBy  { get; protected set; }
     public TIdentity? UpdatedBy { get; protected set; }
-    public string CreatedRole { get; set; }
-    public string UpdatedRole { get; set; }
-    public IsDeleted IsDeleted { get; protected set; } = IsDeleted.UnDelete;
-    public string Version { get; } = Guid.NewGuid().ToString();
+    public string CreatedRole   { get; set; }
+    public string UpdatedRole   { get; set; }
+    public IsDeleted IsDeleted  { get; protected set; } = IsDeleted.UnDelete;
+    public string Version       { get; } = Guid.NewGuid().ToString();
 }
--- Architecture/Core/Karami.Core.Domain/Contracts/Abstracts/BaseEntityQuery.cs
+++ Architecture/Core/Karami.Core.Domain/Contracts/Abstracts/BaseEntityQuery.cs
@@ -6,11 +6,11 @@ namespace Karami.Core.Domain.Contracts.Abstracts;
 
 public abstract class BaseEntityQuery<TIdentity>
 {
-    public required TIdentity Id { get; set; }
-    public required TIdentity CreatedBy { get; set; }
+    public TIdentity Id         { get; set; }
+    public TIdentity CreatedBy  { get; set; }
     public TIdentity? UpdatedBy { get; set; }
-    public required string CreatedRole { get; set; }
-    public string UpdatedRole { get; set; }
-    public IsDeleted IsDeleted { get; set; } = IsDeleted.UnDelete;
-    public string Version { get; } = Guid.NewGuid().ToString();
+    public string CreatedRole   { get; set; }
+    public string UpdatedRole   { get; set; }
+    public IsDeleted IsDeleted  { get; set; } = IsDeleted.UnDelete;
+    public string Version       { get; } = Guid.NewGuid().ToString();
 }
