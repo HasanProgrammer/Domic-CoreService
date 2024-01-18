fa74bbcae5c4a101e29792176056c208a4e7b74b
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-01-18 18:21:14 +0330

    updated base domain event

--- Architecture/Core/Karami.Core.Domain/Contracts/Abstracts/DeleteDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Karami.Core.Domain.Contracts.Interfaces;
-
-namespace Karami.Core.Domain.Contracts.Abstracts;
-
-public abstract class DeleteDomainEvent : IDomainEvent
-{
-    public required DateTime UpdatedAt_EnglishDate { get; init; }
-    public required string UpdatedAt_PersianDate   { get; init; }
-}
