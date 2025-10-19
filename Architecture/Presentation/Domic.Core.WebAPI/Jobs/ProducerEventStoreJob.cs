b3e4d32a77ec3529e356b9d1ac1e8a1d9407d410
Hasan Karami <hasankarami2020313@gmail.com>
2025-10-19 19:01:31 +0330

    update

--- Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventStoreJob.cs
+++ Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventStoreJob.cs
@@ -1,8 +1,4 @@
-using Domic.Core.Domain.Contracts.Interfaces;
-using Domic.Core.Infrastructure.Extensions;
 using Domic.Core.UseCase.Contracts.Interfaces;
-using Domic.Core.WebAPI.Signals;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
@@ -12,70 +8,24 @@ public class ProducerEventStoreJob : IHostedService, IDisposable
 {
     private readonly IExternalMessageBroker _externalMessageBroker;
     private readonly IConfiguration _configuration;
-    private readonly IHubContext<JobsSignal> _hubContext;
-    private readonly IDateTime _dateTime;
-    private readonly IExternalDistributedCache _distributedCache;
 
     private Timer _timer;
 
-    public ProducerEventStoreJob(IExternalMessageBroker externalMessageBroker, IConfiguration configuration,
-        IHubContext<JobsSignal> hubContext, IDateTime dateTime, IExternalDistributedCache distributedCache
-    )
+    public ProducerEventStoreJob(IExternalMessageBroker externalMessageBroker, IConfiguration configuration)
     {
         _externalMessageBroker = externalMessageBroker;
         _configuration = configuration;
-        _hubContext = hubContext;
-        _dateTime = dateTime;
-        _distributedCache = distributedCache;
     }
 
-    public async Task StartAsync(CancellationToken cancellationToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
         _externalMessageBroker.NameOfAction  = nameof(ProducerEventStoreJob);
         _externalMessageBroker.NameOfService = _configuration.GetValue<string>("NameOfService");
-
-        var jobsCacheKey = "Jobs";
-        var currentServiceJob = $"{ _externalMessageBroker.NameOfService }-{ nameof(ProducerEventStoreJob) }";
-        
-        var jobsCache = await _distributedCache.GetCacheValueAsync(jobsCacheKey, cancellationToken);
-
-        if(string.IsNullOrEmpty(jobsCache))
-            await _distributedCache.SetCacheValueAsync(
-                new KeyValuePair<string, string>(jobsCacheKey, new string[]{ currentServiceJob }.Serialize()),
-                cancellationToken: cancellationToken
-            );
-        else
-        {
-            var jobsCollection = jobsCache.DeSerialize<List<string>>();
-
-            if(!jobsCollection.Contains(currentServiceJob))
-                jobsCollection.Add(currentServiceJob);
-
-            await _distributedCache.SetCacheValueAsync(
-                new KeyValuePair<string, string>(jobsCacheKey, jobsCollection.Serialize()),
-                cancellationToken: cancellationToken
-            );
-        }
         
         _timer =
-            new Timer(state => {
-
-                _externalMessageBroker.PublishAsEventSourcingAsync(cancellationToken);
-
-                var nowDateTime = DateTime.UtcNow;
-
-                _hubContext.Clients.Groups("JobsSignalGroup").SendAsync(
-                    new JobsSignalDto {
-                        Service = _externalMessageBroker.NameOfService,
-                        Title = "ProducerEventStoreJob",
-                        StartDate = _dateTime.ToPersianShortDate(nowDateTime),
-                        EndDate = _dateTime.ToPersianShortDate(DateTime.UtcNow)
-                    }.Serialize()
-                    ,
-                    cancellationToken
-                );                 
-
-            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            new Timer(state => _externalMessageBroker.PublishAsEventSourcingAsync(cancellationToken), null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+    
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
--- Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventStreamJob.cs
+++ Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventStreamJob.cs
@@ -1,8 +1,4 @@
-using Domic.Core.Domain.Contracts.Interfaces;
-using Domic.Core.Infrastructure.Extensions;
 using Domic.Core.UseCase.Contracts.Interfaces;
-using Domic.Core.WebAPI.Signals;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
@@ -12,70 +8,24 @@ public class ProducerEventStreamJob : IHostedService, IDisposable
 {
     private readonly IExternalEventStreamBroker _externalEventStreamBroker;
     private readonly IConfiguration _configuration;
-    private readonly IHubContext<JobsSignal> _hubContext;
-    private readonly IDateTime _dateTime;
-    private readonly IExternalDistributedCache _distributedCache;
 
     private Timer _timer;
 
-    public ProducerEventStreamJob(IExternalEventStreamBroker externalEventStreamBroker, IConfiguration configuration,
-        IHubContext<JobsSignal> hubContext, IDateTime dateTime, IExternalDistributedCache distributedCache
-    )
+    public ProducerEventStreamJob(IExternalEventStreamBroker externalEventStreamBroker, IConfiguration configuration)
     {
         _externalEventStreamBroker = externalEventStreamBroker;
         _configuration = configuration;
-        _hubContext = hubContext;
-        _dateTime = dateTime;
-        _distributedCache = distributedCache;
     }
 
-    public async Task StartAsync(CancellationToken cancellationToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
         _externalEventStreamBroker.NameOfAction  = nameof(ProducerEventStreamJob);
         _externalEventStreamBroker.NameOfService = _configuration.GetValue<string>("NameOfService");
 
-        var jobsCacheKey = "Jobs";
-        var currentServiceJob = $"{ _externalEventStreamBroker.NameOfService }-{ nameof(ProducerEventStreamJob) }";
-        
-        var jobsCache = await _distributedCache.GetCacheValueAsync(jobsCacheKey, cancellationToken);
-
-        if(string.IsNullOrEmpty(jobsCache))
-            await _distributedCache.SetCacheValueAsync(
-                new KeyValuePair<string, string>(jobsCacheKey, new string[]{ currentServiceJob }.Serialize()),
-                cancellationToken: cancellationToken
-            );
-        else
-        {
-            var jobsCollection = jobsCache.DeSerialize<List<string>>();
-
-            if(!jobsCollection.Contains(currentServiceJob))
-                jobsCollection.Add(currentServiceJob);
-
-            await _distributedCache.SetCacheValueAsync(
-                new KeyValuePair<string, string>(jobsCacheKey, jobsCollection.Serialize()),
-                cancellationToken: cancellationToken
-            );
-        }
-
         _timer =
-            new Timer(state => {
-
-                _externalEventStreamBroker.PublishAsync(cancellationToken);
-
-                var nowDateTime = DateTime.UtcNow;
-
-                _hubContext.Clients.Groups("JobsSignalGroup").SendAsync(
-                    new JobsSignalDto {
-                        Service = _externalEventStreamBroker.NameOfService,
-                        Title = "ProducerEventStreamJob",
-                        StartDate = _dateTime.ToPersianShortDate(nowDateTime),
-                        EndDate = _dateTime.ToPersianShortDate(DateTime.UtcNow)
-                    }.Serialize()
-                    ,
-                    cancellationToken
-                );
-
-            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            new Timer(state => _externalEventStreamBroker.PublishAsync(cancellationToken), null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+    
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
