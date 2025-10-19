279333c6f2295ecbcfff194c505857050fad6b60
Hasan Karami <hasankarami2020313@gmail.com>
2025-10-19 17:53:06 +0330

    update

--- Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventJob.cs
+++ Architecture/Presentation/Domic.Core.WebAPI/Jobs/ProducerEventJob.cs
@@ -1,4 +1,8 @@
+using Domic.Core.Domain.Contracts.Interfaces;
+using Domic.Core.Infrastructure.Extensions;
 using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.Core.WebAPI.Signals;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
@@ -8,31 +12,70 @@ public class ProducerEventJob : IHostedService, IDisposable
 {
     private readonly IExternalMessageBroker _externalMessageBroker;
     private readonly IConfiguration _configuration;
+    private readonly IHubContext<JobsSignal> _hubContext;
+    private readonly IDateTime _dateTime;
+    private readonly IExternalDistributedCache _distributedCache;
 
     private Timer _timer;
 
-    public ProducerEventJob(IExternalMessageBroker externalMessageBroker, IConfiguration configuration)
+    public ProducerEventJob(IExternalMessageBroker externalMessageBroker, IConfiguration configuration,
+        IHubContext<JobsSignal> hubContext, IDateTime dateTime, IExternalDistributedCache distributedCache
+    )
     {
         _externalMessageBroker = externalMessageBroker;
         _configuration = configuration;
+        _hubContext = hubContext;
+        _dateTime = dateTime;
+        _distributedCache = distributedCache;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
         _externalMessageBroker.NameOfAction  = nameof(ProducerEventJob);
         _externalMessageBroker.NameOfService = _configuration.GetValue<string>("NameOfService");
 
-        #if false
+        var jobsCacheKey = "Jobs";
+        var currentServiceJob = $"{ _externalMessageBroker.NameOfService }-{ nameof(ProducerEventJob) }";
         
+        var jobsCache = await _distributedCache.GetCacheValueAsync(jobsCacheKey, cancellationToken);
+
+        if(string.IsNullOrEmpty(jobsCache))
+            await _distributedCache.SetCacheValueAsync(
+                new KeyValuePair<string, string>(jobsCacheKey, new string[]{ currentServiceJob }.Serialize()),
+                cancellationToken: cancellationToken
+            );
+        else
+        {
+            var jobsCollection = jobsCache.DeSerialize<List<string>>();
+
+            if(!jobsCollection.Contains(currentServiceJob))
+                jobsCollection.Add(currentServiceJob);
+
+            await _distributedCache.SetCacheValueAsync(
+                new KeyValuePair<string, string>(jobsCacheKey, jobsCollection.Serialize()),
+                cancellationToken: cancellationToken
+            );
+        }
+
         _timer =
-            new Timer(state => Task.Run(() => _messageBroker.Publish(cancellationToken), cancellationToken), null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
-        
-        #endif
-        
-        _timer =
-            new Timer(state => _externalMessageBroker.PublishAsync(cancellationToken), null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
-        
-        return Task.CompletedTask;
+            new Timer(state => {
+
+                _externalMessageBroker.PublishAsync(cancellationToken);
+
+                var nowDateTime = DateTime.UtcNow;
+
+                _hubContext.Clients.Groups("JobsSignalGroup").SendAsync(
+                    new JobsSignalDto {
+                        Service = _externalMessageBroker.NameOfService,
+                        Title = "ProducerEventJob",
+                        StartDate = _dateTime.ToPersianShortDate(nowDateTime),
+                        EndDate = _dateTime.ToPersianShortDate(DateTime.UtcNow)
+                    }.Serialize()
+                    ,
+                    cancellationToken
+                );
+
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
